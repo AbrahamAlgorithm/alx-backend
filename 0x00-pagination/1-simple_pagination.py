4be0886e3c5c5de960ab59e1f004b27a8a7b1a1b
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-06-25 13:02:31 +0100

    Simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Server module to paginate a database of popular baby names.
+"""
+
+
+import csv
+import math
+from typing import List
+from 0-simple_helper_function import index_range
