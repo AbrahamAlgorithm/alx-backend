d1fe8c54374f0cae79352e1824bb942808f448ac
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-07-02 23:12:23 +0100

    feat: Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module sets up a basic Flask application with a single route.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """
+    Render the index page with a welcome message.
+    """
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run()
