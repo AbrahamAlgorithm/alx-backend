f82f0b094929c45fee1f3bf57761d5a8fef81517
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-07-03 06:11:35 +0100

    Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+This module sets up a basic Flask application with i18n support using Flask-Babel.
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+class Config:
+    """
+    Configuration class for Flask app.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    """
+    Render the index page.
+    """
+    return render_template('1-index.html')
+
+if __name__ == '__main__':
+    app.run()
