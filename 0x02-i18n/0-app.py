2a24a88ff3eda9f12a23c22a5ce115f08be9f63f
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-07-03 06:19:07 +0100

    Basic Babel setup

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,12 +7,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     """
     Render the index page with a welcome message.
     """
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """
-This module sets up a basic Flask application with i18n support using Flask-Babel.
+This module sets up a basic
+Flask application with i18n
+support using Flask-Babel.
 """
 from flask import Flask, render_template
 from flask_babel import Babel
+
 
 class Config:
     """
@@ -13,10 +16,12 @@ class Config:
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 babel = Babel(app)
+
 
 @app.route('/')
 def index():
@@ -24,6 +29,7 @@ def index():
     Render the index page.
     """
     return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run()
