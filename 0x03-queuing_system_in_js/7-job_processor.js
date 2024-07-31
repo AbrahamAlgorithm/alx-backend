0bc355b086161f121bca03b03e573376382582a4
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-07-31 15:25:57 +0100

    Track progress and errors with Kue: Create the Job processor

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,29 @@
+import kue from 'kue';
+
+// Array of blacklisted phone numbers
+const blacklistedNumbers = ['4153518780', '4153518781'];
+
+// Function to send notification
+function sendNotification(phoneNumber, message, job, done) {
+  job.progress(0, 100);
+
+  if (blacklistedNumbers.includes(phoneNumber)) {
+    return done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+  }
+
+  job.progress(50, 100);
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+
+  done();
+}
+
+// Create a queue with Kue
+const queue = kue.createQueue();
+
+// Process jobs from the push_notification_code_2 queue
+queue.process('push_notification_code_2', 2, (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message, job, done);
+});
+
+console.log('Job processor is ready');
