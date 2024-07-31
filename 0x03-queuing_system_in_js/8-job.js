b66a9653d91cb26ec82a186c25d8c2b58459f265
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-07-31 15:42:04 +0100

    Writing the test for job creation

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,16 +1,30 @@
 import kue from 'kue';
 
-function createPushNotificationsJobs (jobs, queue) {
-  if (!Array.isArray(jobs)) throw new Error('Jobs is not an array');
+function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
 
-  jobs.forEach((jobData) => {
-    const job = queue.create('push_notification_code_2', jobData)
+  jobs.forEach((jobInfo) => {
+    const job = queue.create('push_notification_code_3', jobInfo)
       .save((err) => {
-        if (!err) console.log(`Notification job created: ${job.id}`);
+        if (!err) {
+          console.log(`Notification job created: ${job.id}`);
+        }
       });
 
-    job.on('complete', () => console.log(`Notification job ${job.id} completed`));
-    job.on('failed', () => console.log(`Notification job ${job.id} failed`));
-    job.on('progress', (progress) => console.log(`Notification job ${job.id} ${progress}% complete`));
+    job.on('complete', () => {
+      console.log(`Notification job ${job.id} completed`);
+    });
+
+    job.on('failed', (err) => {
+      console.log(`Notification job ${job.id} failed: ${err}`);
+    });
+
+    job.on('progress', (progress) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
   });
 }
+
+export default createPushNotificationsJobs;
---
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -0,0 +1,47 @@
+import { expect } from 'chai';
+import kue from 'kue';
+import createPushNotificationsJobs from './8-job.js';
+
+describe('createPushNotificationsJobs', () => {
+  let queue;
+
+  before(() => {
+    queue = kue.createQueue();
+    queue.testMode.enter();
+  });
+
+  afterEach(() => {
+    queue.testMode.clear();
+  });
+
+  after(() => {
+    queue.testMode.exit();
+  });
+
+  it('display a error message if jobs is not an array', () => {
+    expect(() => createPushNotificationsJobs('not an array', queue)).to.throw('Jobs is not an array');
+  });
+
+  it('create two new jobs to the queue', () => {
+    const jobs = [
+      {
+        phoneNumber: '4153518780',
+        message: 'This is the code 1234 to verify your account'
+      },
+      {
+        phoneNumber: '4153518781',
+        message: 'This is the code 4562 to verify your account'
+      }
+    ];
+
+    createPushNotificationsJobs(jobs, queue);
+
+    expect(queue.testMode.jobs.length).to.equal(2);
+
+    expect(queue.testMode.jobs[0].type).to.equal('push_notification_code_3');
+    expect(queue.testMode.jobs[0].data).to.eql(jobs[0]);
+
+    expect(queue.testMode.jobs[1].type).to.equal('push_notification_code_3');
+    expect(queue.testMode.jobs[1].data).to.eql(jobs[1]);
+  });
+});
